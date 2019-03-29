ee01ff2009b227a04267215ae89d97fe2124e77a
root <root@DESKTOP-07C9913.localdomain>
2019-03-30 00:07:19 +0800

    auto commit

---
+++ msg/src/async_svr.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* 
+    Class async_svr is a role for endpoints, which behaves like 
+    an asynchronous nonblocking request-reply server.
+*/
+
+class async_svr{
+
+
+};
---
+++ msg/src/caller.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <class protocol>
+class caller{
+
+
+};
