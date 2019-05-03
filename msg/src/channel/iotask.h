07e6b20b47652144637d5ec7cf92b10b5aca0b9a
root <root@DESKTOP-07C9913.localdomain>
2019-05-04 07:24:53 +0800

    auto commit

--- msg/src/channel/iotask.h
+++ msg/src/channel/iotask.h
@@ -15,29 +15,37 @@ namespace msg{
 // Zero application-level copy could be achieved by setting addresses in read/write tasks without copying data into a buffer. 
 // The read_tasks or write_tasks are not responsible for releasing data. User must implement their own subclasses of read_task/write_task with overloaded on_success/on_failure callback functions.
 
+// purely abstract I/O task
 class io_task{
 public:
     virtual ~io_task(){}
     virtual void on_success(int bytes_transferred)=0;
     virtual void on_recoverable_failure()=0;
     virtual void on_peer_closed(){}; // back off, increase back-off time
-    virtual void on_conn_closed(){};
+    virtual void on_conn_closed(){}; 
     virtual iovec* iov()=0;
     virtual int iovcnt()=0;
 };
 
+// io_task that implements scatter read
+// undefined: on_success, on_recoverable_failure, iov, iovcnt
 class read_task : public io_task{
 public:
     virtual ~read_task(){}
     bool try_scatter_input(int fd);
 };
 
+// io_task that implements gather write
+// undefined: on_success, on_recoverable_failure, iov, iovcnt
 class write_task : public io_task{
 public:
     virtual ~write_task(){}
     bool try_gather_output(int fd);
 };
 
+
+// io_task that implements iov() and iocnt() based on vector
+// undefined: on_success, on_recoverable_failure
 class vector_io_task : public io_task{
 public:
     vector_io_task(int nriov, int msglen=0): iovs(nriov), header(msglen){
@@ -55,6 +63,8 @@ public:
     uint64_t header; //msglen
 };
 
+// a hybrid of vector_io_task and write_task
+// undefined: on_success, on_recoverable_failure 
 class vector_write_task : public vector_io_task, public write_task{
 public:
     vector_write_task(int nriov, int msglen): vector_io_task(nriov, msglen){}
@@ -67,6 +77,8 @@ public:
     }
 };
 
+// a readtask that implements iov() and iovcnt() based on one iovec
+// undefined: on_success, on_recoverable_failure 
 class oneiov_read_task : public read_task{
 public:
     oneiov_read_task(void* base, int size){
